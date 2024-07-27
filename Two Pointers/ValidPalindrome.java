123d86968988d337322bfff0e1c036cb54e480ae
praveenakan08 <praveenakandukuri08@gmail.com>
2024-07-27 17:36:36 -0500

    valid palindrome solution

---
+++ Two Pointers/ValidPalindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        // non-alpha numeric - only letters and numbers
+        while (left < right) {
+            while(left < s.length() && !isAlphaNumeric(s.charAt(left))) {
+                left++;
+            }
+
+            while(right >= 0 && !isAlphaNumeric(s.charAt(right))) {
+                right--;
+            }
+
+            if(left < s.length() && right >= 0) {
+                if(Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
+                    left++;
+                    right--;
+                } else {
+                    return false;
+                }
+            } 
+        }
+
+        return true;
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return (Character.isLetter(c) || Character.isDigit(c));
+    }
+}
