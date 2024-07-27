f42134dac2b5514dfd7e58854fc2ead00f1dc81c
praveenakan08 <praveenakandukuri08@gmail.com>
2024-07-27 18:28:01 -0500

    valid anagram solution

---
+++ Arrays & Hashing/ValidAnagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> sMap = new HashMap();
+        Map<Character, Integer> tMap = new HashMap();
+        
+        for(int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return sMap.equals(tMap);
+    }
+}
