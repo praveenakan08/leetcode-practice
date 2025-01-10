2b6bb8ab788c09e8f2584854497dac7e105537ff
Praveena Kandukuri <praveena.kandukuri@copart.com>
2025-01-10 08:01:38 -0600

    Inorder and preorder trees

---
+++ Trees/PreOrderTraversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// TC - O(n), SC - O(1)
+class Solution {
+    private List<Integer> result;
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        result = new ArrayList();
+        preorder(root);
+
+        return result;
+    }
+
+    private void preorder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+
+        result.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
