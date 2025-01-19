529683ca9213e344f22f0c533d2d0db6c42060f4
Praveena Kandukuri <praveena.kandukuri@copart.com>
2025-01-18 19:38:08 -0600

    best time to sell stick

---
+++ Sliding Window/BestTimeToSellStock.java
@@ -0,0 +1,16 @@
+// TC - O(n), SC - O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE, maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < min) {
+                min = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - min);
+            }
+        }
+
+        return maxProfit;
+    }
+}
