8c7e79434c81b7e0841d96b6aaa3ced506a09d72
praveenakan08 <praveenakandukuri08@gmail.com>
2025-03-05 23:26:52 -0600

    2965 sol

--- Arrays & Hashing/2965.java
+++ Arrays & Hashing/2965.java
@@ -5,23 +5,17 @@ class Solution {
         int range = grid.length * grid.length;
         Map<Integer, Integer> map = new HashMap();
 
-        for(int i = 1; i <= range; i++) {
-            map.put(i, 0);
-        }
-
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
-                if(map.get(grid[i][j]) == 1) {
-                    result[0] = grid[i][j];
-                }
-            
-                map.put(grid[i][j], map.get(grid[i][j]) + 1);
+                map.put(grid[i][j], map.getOrDefault(grid[i][j], 0) + 1);
             }
         }
 
         for(int i = 1; i <= range; i++) {
-            if(map.get(i) == 0) {
+            if(!map.containsKey(i)) {
                 result[1] = i;
+            } else if(map.get(i) == 2) {
+                result[0] = i;
             }
         }
 
