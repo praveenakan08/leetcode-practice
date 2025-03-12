394d4cce6dd8514aa0f3d95844b4bf67aea07f45
praveenakan08 <praveenakandukuri08@gmail.com>
2025-03-11 22:21:08 -0500

    2529 sol

---
+++ Arrays & Hashing/2529.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int posCount = 0, negCount = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                posCount++;
+            }
+
+            if(nums[i] < 0) {
+                negCount++;
+            }
+        }
+
+        return Math.max(posCount, negCount);
+    }
+}
