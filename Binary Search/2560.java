02cc0575bed2054d813b414e0a60d9ebb7940c13
praveenakan08 <praveenakandukuri08@gmail.com>
2025-03-17 08:33:51 -0500

    2560 sol2

--- Binary Search/2560.java
+++ Binary Search/2560.java
@@ -15,4 +15,43 @@ class Solution {
         }
         return left; //left == right
     }
+
+    // sol 2
+    public int minCapability(int[] A, int k) {
+        // [2, 3, 5, 9]  k = 2
+        int left = Arrays.stream(A).min().getAsInt(), right = Arrays.stream(A).max().getAsInt(), result = 0;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(checkCapability(A, k, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }  
+
+    private boolean checkCapability(int A[], int k, int max) {
+        int count = 0, i = 0;
+
+        while(i < A.length) {
+            if(A[i] <= max) {
+                count++;
+                i += 2;
+            } else {
+                i += 1;
+            }
+
+            if(count == k) {
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
 }
