a42bf7e78840fa05117b6d67e32cf9114b5f1e9b
praveenakan08 <praveenakandukuri08@gmail.com>
2025-05-19 23:57:03 -0400

    3355 sol

---
+++ Arrays & Hashing/3355.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] freq = new int[n];
+        
+        for(int[] q : queries) {
+            freq[q[0]]++;
+            if(q[1] + 1 < n) {
+                freq[q[1] + 1]--;
+            }
+        }
+        
+        int curFreq = 0;
+        for(int i = 0; i < n; i++) {
+            curFreq += freq[i];
+            if(curFreq < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
