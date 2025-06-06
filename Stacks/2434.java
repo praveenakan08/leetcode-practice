6c35e696405671d883e1b38a30fea5f07386fc76
praveenakan08 <praveenakandukuri08@gmail.com>
2025-06-06 00:21:14 -0500

    2434 sol

--- Stacks/2434.java
+++ Stacks/2434.java
@@ -1,41 +1,36 @@
-import java.util.*;
-
 class Solution {
     public String robotWithString(String s) {
-        Stack<Character> stack = new Stack<>();
-        int[] freq = new int[26];
-        
-        // Count frequency of each character
-        for (char ch : s.toCharArray()) {
+        Stack<Character> stack = new Stack();
+
+        int freq[] = new int[26];
+        for(char ch: s.toCharArray()) {
             freq[ch - 'a']++;
         }
-        
-        StringBuilder t = new StringBuilder();
 
-        for (char ch : s.toCharArray()) {
+        StringBuilder t = new StringBuilder();
+        for(char ch: s.toCharArray()) {
             stack.push(ch);
             freq[ch - 'a']--;
 
-            // Check if we can pop the top of the stack
-            while (!stack.isEmpty() && stack.peek() <= smallestChar(freq)) {
+            while(!stack.isEmpty() && stack.peek() <= smallestChar(freq)) {
                 t.append(stack.pop());
             }
         }
 
-        // Append remaining characters from stack
-        while (!stack.isEmpty()) {
+        while(!stack.isEmpty()) {
             t.append(stack.pop());
         }
 
         return t.toString();
     }
 
-    private char smallestChar(int[] freq) {
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
+    private char smallestChar(int freq[]) {
+        for(int i = 0; i < freq.length; i++) {
+            if(freq[i] > 0) {
                 return (char) ('a' + i);
             }
         }
+
         return 'a';
     }
 }
