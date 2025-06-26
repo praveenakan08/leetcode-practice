d4fc4ebd771526bb67e82738948e6dbe3fcad418
praveenakan08 <praveenakandukuri08@gmail.com>
2025-06-26 00:23:18 -0500

    539 sol revise

--- Arrays & Hashing/539.java
+++ Arrays & Hashing/539.java
@@ -17,3 +17,26 @@ class Solution {
         return Math.min(min, 24 * 60 - minutes[minutes.length - 1] + minutes[0]);
     }
 }
+
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        // 1. convert the times into minutes 
+        // 2. sort the array
+        // 3. check adj. elements if min is found 
+
+        int min = Integer.MAX_VALUE;
+        int minutes[] = new int[timePoints.size()];
+
+        for(int i = 0; i < timePoints.size(); i++) {
+            String[] str = timePoints.get(i).split(":");
+            minutes[i] = 60 * Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
+        } 
+
+        Arrays.sort(minutes);
+        for(int i = 1; i < minutes.length; i++) {
+            min = Math.min(minutes[i] - minutes[i - 1], min);
+        }
+        
+        return Math.min(min, 24 * 60 - minutes[minutes.length - 1] + minutes[0]);
+    }
+}
