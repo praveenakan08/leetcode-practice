93581ad26d6729e17c95edae07e5342e05a5b69c
praveenakan08 <praveenakandukuri08@gmail.com>
2025-06-26 00:03:00 -0500

    2311 sol

--- Greedy/2311.java
+++ Greedy/2311.java
@@ -1,11 +1,21 @@
+// TC - O(n), SC - O(1)
 class Solution {
     public int longestSubsequence(String s, int k) {
-        // greedy 
+        // 1. from right to left -- because the lesser powers of 2, more the length to be considered
+        // 2. if 0, then +count 
+        // 3. if 1, and current power 2 + previous accumulated sum <= k, then consider it, otherwise skip it. 
         int sum = 0, count = 0;
 
-
+        // 0123456
+        // 1001010
+        // 6543210 
         for(int i = s.length() - 1; i >= 0; i--) {
-
+            if(s.charAt(i) == '0') {
+                count++;
+            } else if(sum + Math.pow(2, (s.length() - 1) - i) <= k) {
+                count++;
+                sum += Math.pow(2, (s.length() - 1) - i);
+            }
         }
     
         return count;
