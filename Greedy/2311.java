ed3d8e752e8dc08c34a379cd16712f268606c0d6
praveenakan08 <praveenakandukuri08@gmail.com>
2025-06-25 23:54:48 -0500

    init 2311 sol

---
+++ Greedy/2311.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubsequence(String s, int k) {
+        // greedy 
+        int sum = 0, count = 0;
+
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+
+        }
+    
+        return count;
+    }
+}
