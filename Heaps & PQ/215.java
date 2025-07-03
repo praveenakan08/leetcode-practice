9a3df91a72420e924395f0ffc1e8f0028c920e0c
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-02 23:31:39 -0500

    215 revise

---
+++ Heaps & PQ/215.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(maxHeap.size() < k) {
+                maxHeap.add(nums[i]);
+            } else if(nums[i] > maxHeap.peek()) {
+                maxHeap.poll(); // remove the last min element
+                maxHeap.add(nums[i]);
+            }
+        }
+
+        return maxHeap.poll();
+    }
+}
