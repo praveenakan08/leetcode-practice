98730718db8151de406d4ecc969e12c00a6d64a3
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-16 23:36:38 -0500

    53 sol revise

--- Greedy/MaximumSubArraySum.java
+++ Greedy/MaximumSubArraySum.java
@@ -15,3 +15,21 @@ class Solution {
         return max;
     }
 }
+
+// kadane's algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0, maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+
+            currentSum += nums[i];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
