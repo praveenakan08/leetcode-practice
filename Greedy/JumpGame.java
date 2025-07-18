e1fc42c0cf6d29f1e4250263a27f1bccd6653a35
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-17 23:28:57 -0500

    55 sol revise

--- Greedy/JumpGame.java
+++ Greedy/JumpGame.java
@@ -12,3 +12,18 @@ class Solution {
         return goal == 0;
     }
 }
+
+// TC - O(n), SC - O(1)
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(nums[i] + i >= goal) {
+                goal = i;
+            }
+         }
+
+        return goal == 0;
+    }
+}
