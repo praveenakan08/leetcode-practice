4a5602f03301b22d09195c7ef4e4677c063d2bc6
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-22 23:54:02 -0500

    200 sol

--- Graphs/NoOfIslands.java
+++ Graphs/NoOfIslands.java
@@ -29,3 +29,32 @@ class Solution {
         dfs(visited, i, j - 1, grid);
     }
 }
+class Solution {
+    public int numIslands(char[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
+        
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(!visited[i][j] && grid[i][j] == '1') {
+                    dfs(i, j, grid, visited);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int i, int j, char[][] grid, boolean[][] visited) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] == true || grid[i][j] == '0'){
+            return; 
+        }
+
+        visited[i][j] = true;
+        dfs(i + 1, j, grid, visited);
+        dfs(i - 1, j, grid, visited);
+        dfs(i, j + 1, grid, visited);
+        dfs(i, j - 1, grid, visited);
+    }
+}
