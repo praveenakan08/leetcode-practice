105231685a926d1e5214ec132af270b8f8492fde
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-26 23:34:15 -0500

    695 sol revise

--- Graphs/MaxAreaIslands.java
+++ Graphs/MaxAreaIslands.java
@@ -32,3 +32,35 @@ class Solution {
         dfs(i, j-1, grid, visited);
     }
 }
+
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int result = 0;
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    result = Math.max(dfs(i, j, grid), result);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int dfs(int i, int j, int[][] grid) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+
+        int count = 1;
+        grid[i][j] = 0;
+        
+        count += dfs(i + 1, j, grid);
+        count += dfs(i - 1, j, grid);
+        count += dfs(i, j + 1, grid);
+        count += dfs(i, j - 1, grid);
+
+        return count;
+    }
+}
