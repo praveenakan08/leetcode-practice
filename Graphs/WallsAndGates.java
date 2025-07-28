68ed90213906659fa19e06f578f0bf3161984e32
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-27 23:28:46 -0500

    walls and gates revise

--- Graphs/WallsAndGates.java
+++ Graphs/WallsAndGates.java
@@ -30,3 +30,64 @@ public class Solution {
         }
     }
 }
+
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        // m x n grid
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 0) {
+                    dfs(i, j, grid, 0);
+                }
+            }
+        }
+    }
+
+    private void dfs(int i, int j, int[][] grid, int count) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == -1 || grid[i][j] < count) {
+            return;
+        }
+
+        if(grid[i][j] != 0) {
+            grid[i][j] = Math.min(grid[i][j], count);
+        }
+        count += 1;
+
+        dfs(i + 1, j, grid, count);
+        dfs(i - 1, j, grid, count);
+        dfs(i, j + 1, grid, count);
+        dfs(i, j - 1, grid, count);
+    }
+}
+
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        // m x n grid -- using BFS
+        Queue<int[]> q = new LinkedList();
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 0) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while(!q.isEmpty()) {
+            int[] polled = q.poll();
+            int x = polled[0], y = polled[1];
+
+            for(int[] dir: dirs) {
+                int newX = dir[0] + x;
+                int newY = dir[1] + y;
+                
+                if(newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length &&
+                    grid[newX][newY] == Integer.MAX_VALUE) {
+                    grid[newX][newY] = grid[x][y] + 1;
+                    q.add(new int[]{newX, newY});
+                }   
+            }
+        }
+    }
+}
