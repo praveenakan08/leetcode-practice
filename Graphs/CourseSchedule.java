6430fb9b741acb40562351ed80e7f70a082f5e6c
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-29 23:52:13 -0500

    207 sol revise

--- Graphs/CourseSchedule.java
+++ Graphs/CourseSchedule.java
@@ -41,3 +41,47 @@ class Solution {
         return true;
     }   
 }
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       // cycle detection using dfs
+        Map<Integer, List<Integer>> adj = new HashMap();
+        int[] visited = new int[numCourses];
+
+        for(int i = 0; i < numCourses; i++) {
+            adj.put(i, new ArrayList());
+        }
+
+        // 1 -> 0, 0 -> 1
+        for(int i = 0; i < prerequisites.length; i++) {
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+
+        for(Integer key: adj.keySet()) {
+            if(visited[key] == 0 && !dfs(key, adj, visited)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int key, Map<Integer, List<Integer>> adj, int[] visited) {
+        if(visited[key] == 2) {
+            return false;   // cycle detected
+        }
+
+        if(visited[key] == 1 || adj.get(key).isEmpty()) {
+            return true;    // already visited or if no preq
+        }
+
+        visited[key] = 2;
+        for(int dep: adj.get(key)) {
+            if(!dfs(dep, adj, visited)) {
+                return false;
+            }
+        }
+
+        visited[key] = 1;
+        return true;
+    }
+}
