8cc213a86f26dccaafbeb7950dc6981f06d336a7
praveenakan08 <praveenakandukuri08@gmail.com>
2025-07-31 23:30:10 -0500

    LCA revise

--- Trees/LCA-Recursion.java
+++ Trees/LCA-Recursion.java
@@ -23,3 +23,30 @@ class Solution {
         return root;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // LCA
+        if(root == null) {
+            return root;
+        }
+
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+}
