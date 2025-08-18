cff0295bb2b9b6a605e84abf3d907ebb8874179d
praveenakan08 <praveenakandukuri08@gmail.com>
2025-08-17 23:47:05 -0500

    CS2 sol revise

--- Back Tracking/CombinationSum2.java
+++ Back Tracking/CombinationSum2.java
@@ -35,3 +35,35 @@ class Solution {
         dfs(i + 1, target, current, currentSum, nums);
     }
 }
+class Solution {
+    private List<List<Integer>> result;
+
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        result = new ArrayList();
+
+        dfs(0, 0, new ArrayList(), nums, target);
+        return result;
+    }
+
+    private void dfs(int i, int currentSum, List<Integer> current, int[] nums, int target) {
+        if(currentSum == target) {
+            result.add(new ArrayList(current));
+            return;
+        }
+
+        if(i >= nums.length || currentSum > target) {
+            return;
+        }
+
+        current.add(nums[i]);
+        dfs(i + 1, currentSum + nums[i], current, nums, target);
+
+        current.remove(current.size() - 1);
+        while(i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++;
+        }
+
+        dfs(i + 1, currentSum, current, nums, target);
+    }
+}
