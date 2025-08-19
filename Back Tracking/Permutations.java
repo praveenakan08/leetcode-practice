5cc9d9de123b0d859526fb306c6c3b6924658058
praveenakan08 <praveenakandukuri08@gmail.com>
2025-08-18 23:55:10 -0500

    permutations revise

--- Back Tracking/Permutations.java
+++ Back Tracking/Permutations.java
@@ -26,3 +26,31 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    List<List<Integer>> result;
+
+    public List<List<Integer>> permute(int[] nums) {
+        result = new ArrayList();
+        dfs(new ArrayList(), nums);
+
+        return result;   
+    }
+
+    private void dfs(List<Integer> subset, int nums[]) {
+        if(subset.size() == nums.length) {
+            result.add(new ArrayList(subset));
+            return; 
+        }
+
+        for(int i: nums) {
+            if(subset.contains(i)) {
+                continue;
+            }
+
+            subset.add(i);
+            dfs(subset, nums);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
