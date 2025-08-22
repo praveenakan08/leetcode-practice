d820d351487056ace511e9969bbbb9f1d14eccec
praveenakan08 <praveenakandukuri08@gmail.com>
2025-08-21 23:59:18 -0500

    subsets sups sol

--- Back Tracking/Subsets2.java
+++ Back Tracking/Subsets2.java
@@ -30,3 +30,30 @@ class Solution {
         dfs(i + 1, current, nums);
     }
 }
+
+class Solution {
+    List<List<Integer>> result;
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        result = new ArrayList();
+
+        rec(0, new ArrayList(), nums);
+        return result;    
+    }
+
+    private void rec(int i, List<Integer> current, int[] nums) {
+        if(i >= nums.length) {
+            result.add(new ArrayList(current));
+            return;
+        }
+
+        current.add(nums[i]);
+        rec(i + 1, nums, current);
+
+        current.remove(current.size() - 1);
+        while(i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++;
+        }
+        dfs(i + 1, nums, current);
+    }
+}
