d4d1a9beab010304cf4ade0e6fae89e762c22cd6
praveenakan08 <praveenakandukuri08@gmail.com>
2025-09-08 00:58:27 -0500

    bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -15,3 +15,17 @@ class Solution {
         return nums;
     }
 }
+
+private int[] sort(int[] nums) {
+
+    // swap the largest element found that moment, and place it at the END
+    for(int i = 0; i < nums.length; i++) {
+        for(int j = 0; j < nums.length - 1; j++) {
+            if(nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+}
