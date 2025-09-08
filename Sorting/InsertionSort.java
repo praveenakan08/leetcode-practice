c54691cf878d5362a7e52891addfdd8d1840dedc
praveenakan08 <praveenakandukuri08@gmail.com>
2025-09-08 01:04:27 -0500

    insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -16,3 +16,23 @@ class Solution {
         return nums;
     }
 }
+
+private int[] sort(int[] nums) {
+    
+    // insert the unsorted element at the correct place
+
+    for(int i = 1; i < nums.length; i++) {
+        int key = nums[i], j = i - 1;
+
+        while(j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j];
+            j--;
+        }
+
+
+        // placing the unsorted element at the correct position
+        nums[j + 1] = key;
+    }
+
+    return nums;
+}
