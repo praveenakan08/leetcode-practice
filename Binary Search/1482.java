5c79aca907170dba42b117858591094bffde36f8
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-03 00:48:24 -0500

    1482 BS sol

--- Binary Search/1482.java
+++ Binary Search/1482.java
@@ -33,3 +33,48 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if(m * k > bloomDay.length) {
+            return -1;
+        }
+
+        int left = Integer.MAX_VALUE, right = Integer.MIN_VALUE;
+        for(int i = 0; i < bloomDay.length; i++) {
+            left = Math.min(left, bloomDay[i]);
+            right = Math.max(right, bloomDay[i]);
+        }     
+
+        int result = 0;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(possible(bloomDay, mid, m, k)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean possible(int[] bloomDay, int day, int m, int k) {
+        int count = 0, result = 0;
+        
+        for(int i = 0; i < bloomDay.length; i++) {
+            if(bloomDay[i] <= day) {
+                count++; 
+            } else {
+                result += count / k;
+                count = 0;
+            }
+        }
+
+        result += count / k;
+        return result >= m;
+    } 
+}
+
