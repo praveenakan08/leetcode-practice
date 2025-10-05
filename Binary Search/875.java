ad25ae07703d830371385ee53193be74313cc287
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-04 22:29:57 -0500

    875 BS sol

--- Binary Search/875.java
+++ Binary Search/875.java
@@ -73,3 +73,62 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        // n -> piles
+        // h -> hours
+        // bananas per hour -> k
+
+        // 1. calculate total
+        // 2. from hour 1 to hour 8
+        // 3. min k = initial is min of piles
+        
+        // [3, 6, 7, 11]
+        // k = 3
+
+        // hour 1 -> [0, 6, 7, 11]
+        // hour 2 -> [0, 3, 7, 11]
+        // hour 3 -> [0, 0, 7, 11]
+        // hour 4 -> [0, 0, 4, 11]
+        // hour 5 -> [0, 0, 1, 11]
+        // hour 6 -> [0, 0, 0, 11]
+        // hour 7 -> ... cannot finish all
+
+        // k = 4
+        // h1 -> [0, 6, 7, 11]
+        // h2 -> [0, 2, 7, 11]
+        // h3 -> [0, 0, 3, 11]
+        // h4 -> [0, 0, 0, 11]
+        // h5 -> [0, 0, 0, 7]
+        // h6 -> [0, 0, 0, 3]
+        // h7 -> [0, 0, 0, 0]
+
+        int left = 1, right = 1;
+        for(int i = 0; i < piles.length; i++) {
+            right = Math.max(right, piles[i]);
+        }
+
+        while(left < right) {
+            int mid = (left + right) / 2;
+
+            if(findK(mid, piles, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+
+    private boolean findK(int k, int[] piles, int h) {
+        double totalHours = 0;
+
+        for(int i = 0; i < piles.length; i++) {
+            totalHours += Math.ceil((double) piles[i] / k);
+        }
+        
+        return totalHours <= h;
+    }
+}
