0e8e44e5975c7107eb73234c73e3f12a58d6fa00
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-15 23:58:54 -0500

    846 sol

--- Heaps & PQ/846.java
+++ Heaps & PQ/846.java
@@ -56,3 +56,55 @@ class Solution {
         return true;
     }
 }
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) {
+            return false;
+        }
+
+        Queue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        Map<Integer, Integer> map = new HashMap();
+        for(int i = 0; i < hand.length; i++) {
+            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
+        }
+
+        for(int key: map.keySet()) {
+            heap.add(new int[]{key, map.get(key)});
+        }
+
+        // 1,2,3,6,2,3,4,7,8
+        // 2,3,4,6,7,8
+        // [1, 2, 3], [2, 3, 4]
+        while(!heap.isEmpty()) {
+            List<int[]> spare = new ArrayList();
+            int[] first = heap.peek();
+            int start = first[0];
+
+            for (int i = 0; i < groupSize; i++) {
+                if (heap.isEmpty()) return false;
+
+                int[] polled = heap.remove();
+
+                int count = polled[1], value = polled[0];
+                if(value != start + i) {
+                    return false;
+                }
+
+                count--;
+                if(count > 0) {
+                    spare.add(new int[]{polled[0], count});
+                }
+
+            }
+
+            heap.addAll(spare);
+        }
+
+        return true;
+    }
+}
