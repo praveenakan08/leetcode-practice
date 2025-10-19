3a5b7d793c30521f7c48a605961a6b9ddcdae515
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-19 17:25:13 -0500

    jump 2 sol revise

--- Greedy/JumpGame2.java
+++ Greedy/JumpGame2.java
@@ -19,3 +19,26 @@ class Solution {
         return count;
     }
 }
+class Solution {
+    public int jump(int[] nums) {
+        int min = 0, farthest = 0, left = 0, right = 0;
+
+        // [2,3,1,1,4]
+        // [0,1,2,3,4]
+        // left = 0, right = 0, farthest = 0, min = 0
+        while(right < nums.length - 1) {
+            farthest = 0;
+
+            for(int i = left; i <= right; i++) {
+                farthest = Math.max(farthest, nums[i] + i);
+            }
+
+            left = right + 1;
+            right = farthest;
+
+            min++;
+        }
+
+        return min;
+    }
+}
