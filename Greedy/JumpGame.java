c35ca74248ced6d8ec790bf470bc402863880ba5
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-19 16:53:07 -0500

    jump sol revise

--- Greedy/JumpGame.java
+++ Greedy/JumpGame.java
@@ -41,3 +41,29 @@ class Solution {
         return goal == 0;
     }
 }
+
+// TC - O(n), SC - O(1)
+class Solution {
+    public boolean canJump(int[] nums) {
+        // another approach
+        // 1. find the max index you can reach
+        // 2. if current is > max index that means it cannot be reached, return false
+
+        int maxIndex = 0;
+
+        // [3,2,1,0,4]
+        // [0,1,2,3,4]
+
+        // max = 3
+        for(int i = 0; i < nums.length; i++) {
+            if(i > maxIndex) {
+                return false;
+            }
+
+            maxIndex = Math.max(maxIndex, nums[i] + i);
+        }
+
+        return true;
+        
+    }
+}
