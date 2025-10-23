2c00f891f6fa170279e81f744126d0d29bd08f7a
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-22 23:07:51 -0500

    435 sol

--- Greedy/678.java
+++ Greedy/678.java
@@ -30,3 +30,38 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> openStack = new Stack();
+        Stack<Integer> starStack = new Stack();
+
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                openStack.push(i);
+            } else if(s.charAt(i) == '*') {
+                starStack.push(i);
+            } else {
+                if(!openStack.isEmpty()) {
+                    openStack.pop();
+                } else if(!starStack.isEmpty()) {
+                    starStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while(!starStack.isEmpty() && !openStack.isEmpty()) {
+            if(openStack.peek() > starStack.peek()) {
+                return false;
+            } 
+
+            openStack.pop();
+            starStack.pop();
+        }
+        
+        return true;
+    }
+}
