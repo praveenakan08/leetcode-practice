bd124c6dc0f56a622311676892c0a10632d51643
praveenakan08 <praveenakandukuri08@gmail.com>
2025-10-22 21:37:25 -0500

    435 sol revise

--- Greedy/435.java
+++ Greedy/435.java
@@ -17,3 +17,24 @@ class Solution {
         return result;
     }
 }
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        // sort by start
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int count = 0;
+
+        // [1, 2], [1, 3], [2, 3], [3, 4]
+        for(int i = 1; i < intervals.length; i++) {
+            if(intervals[i - 1][1] > intervals[i][0]) {     // overlapping
+                count++;
+
+                // update the current interval with the not removed one
+                intervals[i][0] = intervals[i - 1][0];
+                intervals[i][1] = Math.min(intervals[i - 1][1], intervals[i][1]);
+            }   
+        }
+        
+        return count;
+    }
+}
